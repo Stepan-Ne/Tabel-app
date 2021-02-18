634292754d7c62ccdada580cf6d0f3796a08680e
Stepan <nemail.necom@gmail.com>
2021-02-18 22:36:45 +0300

    card

--- src/components/Table/BasicTable.jsx
+++ src/components/Table/BasicTable.jsx
@@ -164,15 +164,16 @@ const BasicTable = ({ tableData }) => {
         {selectedFlatRows.map((row) => (
           <div className='selectedRow'>
             <ul>
-              <li>Выбран пользователь: {row.original.firstName}{' '}
-              {row.original.lastName}
+              <li>Выбран пользователь: <b>{row.original.firstName}{' '}
+              {row.original.lastName}</b>
               </li>
               <li> Описание:
               <textarea value={row.original.description}></textarea>
               </li>
-              <li></li>
-              <li></li>
-              <li></li>
+              <li>Адрес проживания: <b>{row.original.address.streetAddress}</b></li>
+              <li>Город: <b>{row.original.address.city}</b></li>
+              <li>Провинция/штат: <b>{row.original.address.state}</b></li>
+              <li> Индекс: <b>{row.original.address.zip}</b> </li>
             </ul>
             </div>
             
